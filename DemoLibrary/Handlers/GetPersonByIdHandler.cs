5201cece2cb0b6d8035bf70ee13bf329b3c9088c
Mathews Nyirongo <mathews.chiza@gmail.com>
2023-12-22 08:58:37 +0400

    Add project files.

---
+++ DemoLibrary/Handlers/GetPersonByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonByIdHandler : IRequestHandler<GetPersonByIdQuery, PersonModel>
+    {
+        private readonly IMediator mediator;
+
+        public GetPersonByIdHandler(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        public async Task<PersonModel> Handle(GetPersonByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await mediator.Send(new GetPersonListQuery());
+            return result.FirstOrDefault(x => x.Id == request.Id);
+        }
+    }
+}
